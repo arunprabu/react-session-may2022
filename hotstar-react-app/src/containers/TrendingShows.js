a92ad8d1a36d5f0ff6828ed2b90ae90a845665fc
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-05-13 18:44:13 +0530

    updated

--- hotstar-react-app/src/containers/TrendingShows.js
+++ hotstar-react-app/src/containers/TrendingShows.js
@@ -12,8 +12,9 @@ class TrendingShows extends Component {
           category: 'Infotainment',
           releasedOn: 'Last Week',
           duration: '40 mins',
+          thumbnail: 'https://via.placeholder.com/250x180?text=PlanetEarth',
           desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 
-          isInWatchlist: false
+          isInWatchlist: true
         },
         {
           id: 2, 
@@ -21,6 +22,7 @@ class TrendingShows extends Component {
           category: 'Infotainment',
           releasedOn: 'Today',
           duration: '60 mins',
+          thumbnail: 'https://via.placeholder.com/250x180?text=PlanetEarthII',
           desc: 'Nam maximus augue ac lacinia feugiat.', 
           isInWatchlist: false 
         },
@@ -30,6 +32,7 @@ class TrendingShows extends Component {
           category: 'Sports',
           releasedOn: 'Today',
           duration: '4 hours',
+          thumbnail: 'https://via.placeholder.com/250x180?text=IPL2022',
           desc: 'Vivamus vitae blandit justo, eu feugiat quam.', 
           isInWatchlist: false 
         },
@@ -39,6 +42,7 @@ class TrendingShows extends Component {
           category: 'Infotainment',
           releasedOn: 'Today',
           duration: '45 mins',
+          thumbnail: 'https://via.placeholder.com/250x180?text=ManVsWild',
           desc: ' Suspendisse tincidunt rutrum mattis.', 
           isInWatchlist: false 
         }
@@ -46,39 +50,62 @@ class TrendingShows extends Component {
     }
   }
 
+  handleAddToWatchlist( index ){
+    // working with state immutably
+    let shows = [ ...this.state.showList ]; // shallow copy
+    shows[index].isInWatchlist = !shows[index].isInWatchlist; // toggling  isInWatchlist
+    this.setState({
+      showList: shows
+    });
+  }
 
   render() {
-    return (
-      <div className="row">
-        <div className="col-md-3">
-          <div className="card" >
-            <img src="https://via.placeholder.com/250x180?text=NatGeo" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Trending Show name</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </p>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">Duration: 40 mins</li>
-              <li className="list-group-item">Category: Infotainment</li>
-              <li className="list-group-item">Released Today</li>
-            </ul>
-            <div className="card-body">
-              <button type='button' className='btn btn-sm btn-primary'>
-                Watch Now
-              </button>
-              <button type='button' className='float-end btn btn-sm btn-success'>
-                Add to Watchlist
-              </button>
+
+    // ideal place for you to loop thru using map -- lists
+    // ideal place for any comp specific transformations in JSX
+    let trendingShowList = null;
+
+    if(Array.isArray(this.state.showList)){
+      trendingShowList = this.state.showList.map( (show, index) => {
+        return( 
+          <div className="col-md-3" key={show.id}>
+            <div className="card" >
+              <img src={show.thumbnail} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{show.name}</h5>
+                <p className="card-text">{show.desc}</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Duration: {show.duration}</li>
+                <li className="list-group-item">Category: {show.category}</li>
+                <li className="list-group-item">Released {show.releasedOn}</li>
+              </ul>
+              <div className="card-body">
+                <button type='button' className='btn btn-sm btn-primary'>
+                  Watch Now
+                </button>
+                <button type='button' className='float-end btn btn-sm btn-success'
+                  onClick={ this.handleAddToWatchlist.bind(this, index) }>
+                  {show.isInWatchlist? 'In Watchlist': 'Add to Watchlist'}
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className='alert alert-warning'>
-          No Trending Shows Found
-        </div>
+        )
+      });
+    }
+    
+    return (
+      <div className="row">
+        {/* Conditionals */}
+        { 
+          this.state.showList && this.state.showList.length > 0? 
+            trendingShowList 
+            :
+            <div className='alert alert-warning'>
+              No Trending Shows Found!
+            </div>
+        }
       </div>
     )
   }
